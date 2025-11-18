ad782912349f153089124326268e31d7eb75980e
Alan Bond <bonddevconsult@gmail.com>
2025-11-18 12:59:23 -0500

    All links lead to form

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function scrollToWaitlist() {
+  document.getElementById('early-access')?.scrollIntoView({
+    behavior: 'smooth',
+    block: 'center'
+  });
+}
